163d4c6c8f32aab833b3359ce438b0b0389a2f51
k.sokolov1 <k.sokolov@netology.tech>
2025-09-27 18:32:06 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
-    public static int add(String name, String number) {
-        return 0;
+    private final Map<String, String> nameToNumber;    // Для поиска по имени
+
+    public PhoneBook() {
+        nameToNumber = new HashMap<>();
+    }
+
+    public int add(String name, String number) {
+        if (nameToNumber.containsKey(name)) {
+            return nameToNumber.size(); // Возвращаем текущее количество
+        }
+
+        nameToNumber.put(name, number);
+
+        return nameToNumber.size();
     }
 }
