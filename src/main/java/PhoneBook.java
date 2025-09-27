45ae2418dfcbc6bc07cca8e6a3b9184c35871a63
k.sokolov1 <k.sokolov@netology.tech>
2025-09-27 18:37:11 +0300

    findByNumber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,9 +4,11 @@ import java.util.Map;
 public class PhoneBook {
 
     private final Map<String, String> nameToNumber;    // Для поиска по имени
+    private final Map<String, String> numberToName;    // Для поиска по номеру
 
     public PhoneBook() {
         nameToNumber = new HashMap<>();
+        numberToName = new HashMap<>();
     }
 
     public int add(String name, String number) {
@@ -15,11 +17,12 @@ public class PhoneBook {
         }
 
         nameToNumber.put(name, number);
+        numberToName.put(number, name);
 
         return nameToNumber.size();
     }
 
     public String findByNumber(String number) {
-        return null;
+        return numberToName.get(number);
     }
 }
